2e7f69c534b74d8860bad15c4d62885da9300187
Rumba13 <cveremeev@yandex.by>
2024-09-29 15:10:44 +0300

    feat: add simple create-product-form

--- src/features/create-apartment/ui/create-apartment-form.tsx
+++ src/features/create-apartment/ui/create-apartment-form.tsx
@@ -1,8 +1,30 @@
 import './styles.scss';
-import {Form, Formik} from "formik";
+import {Form, Formik, FormikValues} from "formik";
 import React from "react";
 import {Field} from "../../../shared/ui/field/ui";
 import {useTypedTranslation} from "../../../app/i18n/use-typed-translation";
+import {RangeInput} from "../../../shared/ui/range-input/ui";
+
+type FormValuesType = {
+
+}
+
+const validate = (values:FormikValues) => {
+    const errors:any = {}
+console.log(values.roomsQuantity)
+
+    if(values.roomsQuantity === ""){
+        errors.roomsQuantity = "Required"
+    }
+    else if(values.roomsQuantity !== Math.trunc(values.roomsQuantity)){
+        errors.roomsQuantity = "Should be integer"
+    }
+    else if(!Number.isInteger(values.roomsQuantity)){
+        errors.roomsQuantity = "Should be number"
+    }
+
+    return errors;
+}
 
 export function CreateApartmentForm() {
     const {t} = useTypedTranslation();
@@ -10,20 +32,21 @@ export function CreateApartmentForm() {
     return <div className="create-apartment-form-wrapper">
         <span className="create-apartment-form-wrapper__title">{t("Create Apartment")}</span>
 
-        <Formik initialValues={{title: "sdaddad"}}
+        <Formik<FormikValues> initialValues={{title: ""}} validate={validate}
                 onSubmit={(values, {setSubmitting}) => alert(JSON.stringify(values, null, 2))}>
             {({}) => (
-                <Form className={"create-apartment-form"}>
+                <Form className="create-apartment-form">
                     <Field placeholder={"Название квартиры"} name="title" label={t("Title")}/>
                     <Field name="description" as="textarea"  label={t("Apartment Description")}/>
-                    <Field name="roomsQuantity" label={t("Rooms Quantity")}/>
-                    <Field name="bedsQuantity" label={t("Beds Quantity")}/>
-                    <Field name="guestsQuantity" label={t("Guests")}/>
-                    <Field name="price" label={t("Price")}/>
+                    <Field name="roomsQuantity" type="number" label={t("Rooms Quantity")}/>
+                    <Field name="bedsQuantity" type="number" label={t("Beds Quantity")}/>
+                    <Field name="guestsQuantity" type="number" label={t("Guests")}/>
+
+                    <Field name="price" type="number" label={t("Price")}/>
                     <Field name="address" label={t("Address")}/>
                     <Field name="area" label={t("Area")}/>
                     <Field name="amenities" label={t("Amenities")}/>
-                    <Field name="photos" label={t("Photos")}/>
+                    <Field name="photos" type="file" accept={"image/*"} label={t("Photos")} multiple/>
                     <button className={"create-apartment-form__submit"} type="submit">{t("Add")}</button>
                 </Form>
             )}</Formik>
