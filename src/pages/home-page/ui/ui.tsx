0d595ae5da34aad74b9b138655b4d3c5b5abd0d4
NotebookSenderium <senderium1@gmail.com>
2024-11-05 00:21:40 +0300

    fix: fix welcome modals opening after redirection to home page

--- src/pages/home-page/ui/ui.tsx
+++ src/pages/home-page/ui/ui.tsx
@@ -4,22 +4,19 @@ import {ApartmentList} from "../../../widgets/apartment-list";
 import {SortBy} from "../../../features/sort-by";
 import {OpenCreateApartmentPageButton} from "./open-create-apartment-page-button";
 import {userStore} from "../../../entities/user";
-import {useEffect, useRef} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import {observer} from "mobx-react";
 import {Pagination} from "antd";
 import {apartmentListStore} from "../../../widgets/apartment-list/model/apartment-list-store";
 import {SelectDatesModal, selectDatesModalStore} from "../../../widgets/welcome-modal";
-import {selectGuestModalStore, SelectGuestsFormModal, SelectGuestsModal} from "../../../widgets/select-guests-modal";
-import {Formik} from "formik";
+import {selectGuestModalStore, SelectGuestsModal} from "../../../widgets/select-guests-modal";
 import {guestsCountStore} from "../../../features/FILTER/filter-by-guests";
+//For exact 1 execution
+selectDatesModalStore.open()
 
 export const HomePage = observer(() => {
     useEffect(() => {
     }, [userStore.user]);
-
-    useEffect(() => {
-        selectDatesModalStore.open()
-    }, []);
 
     const scrollToRef = useRef<HTMLDivElement>(null);
 
