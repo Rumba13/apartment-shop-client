f485f29bddf5c1d92eeecc2c99c5b1ae700918d9
Rumba13 <cveremeev@yandex.by>
2024-12-01 11:51:27 +0300

    feat(favorites page): skeleton count depends on favorites count

--- src/features/select-tags/ui/ui.tsx
+++ src/features/select-tags/ui/ui.tsx
@@ -3,12 +3,12 @@ import { observer } from "mobx-react";
 import { CheckBox } from "../../../shared/ui/check-box/ui";
 import { useEffect } from "react";
 import { tagsFilterStore } from "../model/tags-filter-store";
-import { useTypedTranslation } from "../../../app/i18n/use-typed-translation";
-import { Skeleton } from "antd";
 import { TagSkeleton } from "./tag-skeleton";
+import { useTranslation } from "react-i18next";
+import { favoritesStore } from "../../APARTMENT/add-apartment-to-favorites/model/favorites-store";
 
 export const TagsList = observer(() => {
-   const { t } = useTypedTranslation();
+   const { t } = useTranslation();
 
    useEffect(() => {
       tagsFilterStore.loadTags();
@@ -18,7 +18,6 @@ export const TagsList = observer(() => {
       return (
          <div className="tags">
             <span className="filters__title tags__title">{t("Amenities")}</span>
-
             <TagSkeleton textSkeletonWidth="80%" />
             <TagSkeleton textSkeletonWidth="95%" />
             <TagSkeleton textSkeletonWidth="10%" />
@@ -35,9 +34,10 @@ export const TagsList = observer(() => {
       <div className="tags">
          <span className="filters__title tags__title">{t("Amenities")}</span>
 
-         {tagsFilterStore.tags.map(tag => (
-            <CheckBox value={tagsFilterStore.selectedTags[tag]} name={tag} key={tag} onValueChange={() => tagsFilterStore.toggleIsTagSelected(tag)} />
-         ))}
+            {tagsFilterStore.tags.map(tag => (
+               <CheckBox value={tagsFilterStore.selectedTags[tag]} name={tag} key={tag}
+                         onValueChange={() => tagsFilterStore.toggleIsTagSelected(tag)} />
+            ))}
       </div>
    );
 });
--- src/widgets/favorite-list/ui/ui.tsx
+++ src/widgets/favorite-list/ui/ui.tsx
@@ -2,10 +2,7 @@ import "./styles.scss";
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
 import { ApartmentCard, ApartmentCardSkeleton } from "../../../entities/apartment-card";
-import { Apartment } from "../../../shared/api/types/apartment";
-import { apartmentService } from "../../../shared/api/apartment-service";
 import { currencyStore } from "../../../features/select-currency";
-import { setPhotosAbsolutePath } from "../../../shared/lib/set-photos-absolute-path";
 import { favoritesStore } from "../../../features/APARTMENT/add-apartment-to-favorites/model/favorites-store";
 import { favoriteListStore } from "../model/favorite-list-store";
 
@@ -16,14 +13,15 @@ export const FavoriteList = observer(({}: PropsType) => {
       favoriteListStore.loadFavoriteList(favoritesStore.favorites);
    }, [favoritesStore.favorites, currencyStore.currency]);
 
+   console.log(favoritesStore.favoritesCount)
+
    if (favoriteListStore.isLoading) {
+
       return (
          <div className="favorite-list">
-            <ApartmentCardSkeleton />
-            <ApartmentCardSkeleton />
-            <ApartmentCardSkeleton />
-            <ApartmentCardSkeleton />
-            <ApartmentCardSkeleton />
+            {Array.from({ length: favoritesStore.favoritesCount }, (_, i) => (
+               <ApartmentCardSkeleton key={i} />
+            ))}
          </div>
       );
    }
